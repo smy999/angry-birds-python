b35e80a9a8627bd34a4ac343cedf3dd78bec1d1c
smy999 <33407191+smy999@users.noreply.github.com>
2017-12-10 09:59:35 +0900

    Update characters.py

--- src/characters.py
+++ src/characters.py
@@ -22,6 +22,47 @@ class Bird():
         self.body = body
         self.shape = shape
 
+class Blue_Bird():
+    def __init__(self, distance, angle, x, y, space):
+        self.life = 20
+        mass = 5
+        radius = 12
+        inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+        body = pm.Body(mass, inertia)
+        body.position = x, y
+        # power down
+        power = distance * 33
+        impulse = power * Vec2d(1, 0)
+        angle = -angle
+        body.apply_impulse_at_local_point(impulse.rotated(angle))
+        shape = pm.Circle(body, radius, (0, 0))
+        shape.elasticity = 0.95
+        shape.friction = 1
+        shape.collision_type = 10
+        space.add(body, shape)
+        self.body = body
+        self.shape = shape
+
+class Big_Bird():
+    def __init__(self, distance, angle, x, y, space):
+        self.life = 20
+        mass = 5
+        radius = 20
+        inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+        body = pm.Body(mass, inertia)
+        body.position = x, y
+        # power up
+        power = distance * 3000
+        impulse = power * Vec2d(1, 0)
+        angle = -angle
+        body.apply_impulse_at_local_point(impulse.rotated(angle))
+        shape = pm.Circle(body, radius, (0, 0))
+        shape.elasticity = 0.95
+        shape.friction = 1
+        shape.collision_type = 12
+        space.add(body, shape)
+        self.body = body
+        self.shape = shape
 
 class Pig():
     def __init__(self, x, y, space):
@@ -35,6 +76,22 @@ class Pig():
         shape.elasticity = 0.95
         shape.friction = 1
         shape.collision_type = 1
+        space.add(body, shape)
+        self.body = body
+        self.shape = shape
+
+class Pig_Big():
+    def __init__(self, x, y, space):
+        self.life = 100
+        mass = 5
+        radius = 50
+        inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+        body = pm.Body(mass, inertia)
+        body.position = x, y
+        shape = pm.Circle(body, radius, (0, 0))
+        shape.elasticity = 0.95
+        shape.friction = 1
+        shape.collision_type = 13
         space.add(body, shape)
         self.body = body
         self.shape = shape
